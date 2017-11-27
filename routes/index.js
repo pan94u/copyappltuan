3d865a4bc86bab029bd91b4ec0929ffdb668e9b5
pan9 <438063442@qq.com>
2017-11-27 17:37:55 +0800

    设置缓存
    报价第一次获取后进行缓存，有效时间一小时，一小时后再次请求将重新获取报价

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 var cheerio = require('cheerio');
-
+var path;
+var result = {};
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {
@@ -12,16 +13,16 @@ router.get('/', function(req, res, next) {
 
 router.get('/getJobs', function(req, res, next) {
 	var Res = res;
+	var get_url = 'http://www.appletuan.com/page/about';
 	var url = 'http://www.appletuan.com';
-	var path;
 	var cid = req.param('cid');
 	if (cid) {
-		cid = "?cid=" + cid
+		cid = "?cid=" + cid;
 	} else {
 		cid = "";
 	}
 
-	http.get(url, function(res) {
+	http.get(get_url, function(res) {
 		var chunks = [];
 		var size = 0;
 		res.on('data', function(chunk) {
@@ -29,33 +30,41 @@ router.get('/getJobs', function(req, res, next) {
 			size += chunk.length;
 		});
 		res.on('end', function() {
-			var result = {};
 			var data = Buffer.concat(chunks, size);
 			var html = data.toString();
 			var $ = cheerio.load(html);
 			var date = $('#main-nav-price-report').text();
-			path = $('#main-nav-price-report').attr('href');
-			console.log('get path success!path:' + path);
-			result["path"] = path;
-			result["date"] = date;
-			http.get(url + path + cid, function(res) {
-				var chunks = [];
-				var size = 0;
-				res.on('data', function(chunk) {
-					chunks.push(chunk);
-					size += chunk.length;
-				})
-				res.on('end', function() {
-					var data = Buffer.concat(chunks, size);
-					var html = data.toString();
-					var $ = cheerio.load(html);
-					var content = $('.col-md-8 .box').eq(1).html();
-					result["html"] = content;
-					Res.json({
-						result: result
+			if (path == $('#main-nav-price-report').attr('href') && (Date.now() - result["time"]) < 3600000) {
+				console.log('getting old data success!');
+				Res.json({
+					result: result
+				});
+			} else {
+				result = {};
+				path = $('#main-nav-price-report').attr('href');
+				console.log('path update success!path:' + path);
+				result["path"] = path;
+				result["date"] = date;
+				result["time"] = Date.now();
+				http.get(url + path + cid, function(res) {
+					var chunks = [];
+					var size = 0;
+					res.on('data', function(chunk) {
+						chunks.push(chunk);
+						size += chunk.length;
+					})
+					res.on('end', function() {
+						var data = Buffer.concat(chunks, size);
+						var html = data.toString();
+						var $ = cheerio.load(html);
+						var content = $('.col-md-8 .box').eq(1).html();
+						result["html"] = content;
+						Res.json({
+							result: result
+						});
 					});
 				});
-			});
+			}
 		});
 	});
 
