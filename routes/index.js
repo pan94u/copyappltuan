8e76160fdd0cebfc68bc569ca9a3e9151ac7fd33
pan9 <438063442@qq.com>
2017-11-27 13:52:47 +0800

    更改报价获取方式
    报价获取链接又原来的计算日期差额修改为首页实时获取- -，增加日期显示

--- routes/index.js
+++ routes/index.js
@@ -11,41 +11,53 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/getJobs', function(req, res, next) {
-	var page = req.param('page');
+	var Res = res;
+	var url = 'http://www.appletuan.com';
+	var path;
 	var cid = req.param('cid');
 	if (cid) {
 		cid = "?cid=" + cid
 	} else {
 		cid = "";
 	}
-	console.log("page:" + page);
-	var Res = res;
-	var url = 'http://www.appletuan.com/daily_price_reports/'
-	http.get(url + page + cid, function(res) {
+
+	http.get(url, function(res) {
 		var chunks = [];
 		var size = 0;
 		res.on('data', function(chunk) {
 			chunks.push(chunk);
 			size += chunk.length;
-		})
+		});
 		res.on('end', function() {
-			var result = [];
+			var result = {};
 			var data = Buffer.concat(chunks, size);
 			var html = data.toString();
 			var $ = cheerio.load(html);
-			if ($('.col-md-8 .box .cell').eq(0).text().indexOf('404') > 0) {
-				result.push("404");
-			} else {
-				var content = $('.col-md-8 .box').eq(1).html();
-				result.push(content);
-			}
-			Res.json({
-				result: result
+			var date = $('#main-nav-price-report').text();
+			path = $('#main-nav-price-report').attr('href');
+			console.log('get path success!path:' + path);
+			result["path"] = path;
+			result["date"] = date;
+			http.get(url + path + cid, function(res) {
+				var chunks = [];
+				var size = 0;
+				res.on('data', function(chunk) {
+					chunks.push(chunk);
+					size += chunk.length;
+				})
+				res.on('end', function() {
+					var data = Buffer.concat(chunks, size);
+					var html = data.toString();
+					var $ = cheerio.load(html);
+					var content = $('.col-md-8 .box').eq(1).html();
+					result["html"] = content;
+					Res.json({
+						result: result
+					});
+				});
 			});
 		});
-	}).on('error', function(err) {
-		console.log(err);
-	});;
+	});
 
 });
 
