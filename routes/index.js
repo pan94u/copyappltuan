8d554912880343b4460294eb4e6b437ce2887d41
sunzeqi <438063442@qq.com>
2018-01-19 00:44:42 +0800

    新增价格趋势图

--- routes/index.js
+++ routes/index.js
@@ -6,90 +6,99 @@ var path;
 var result = {};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: '胖胖数码今日报价'
-    });
+	res.render('index', {
+		title: '胖胖数码今日报价'
+	});
+});
+router.get('/charts', function(req, res, next) {
+
 });
 
 router.get('/product_models/:id/prices', function(req, res, next) {
-    var Res = res;
-    var chart_url = 'http://www.appletuan.com/product_models/' + req.param('id') + '/prices.json?tag=&du=' + req.param('type');
-    http.get(chart_url, function(res) {
-        var chunks = [];
-        var size = 0;
-        res.on('data', function(chunk) {
-            chunks.push(chunk);
-            size += chunk.length;
-        });
-        res.on('end', function() {
-            var data = Buffer.concat(chunks, size);
-            var result = data.toString();
-            Res.json({
-                result: result
-            });
-        });
-    })
-})
+	res.render('charts', {
+		title: '胖胖数码 - 趋势图'
+	});
+});
+
+router.get('/getCharts', function(req, res, next) {
+	var Res = res;
+	var chart_url = 'http://www.appletuan.com/product_models/' + req.param('id') + '/prices.json?tag=&du=' + req.param('du');
+	http.get(chart_url, function(res) {
+		var chunks = [];
+		var size = 0;
+		res.on('data', function(chunk) {
+			chunks.push(chunk);
+			size += chunk.length;
+		});
+		res.on('end', function() {
+			var data = Buffer.concat(chunks, size);
+			var result = data.toString();
+			Res.json({
+				result: result
+			});
+		});
+	})
+});
 
 router.get('/getJobs', function(req, res, next) {
-    var Res = res;
-    var get_url = 'http://www.appletuan.com/page/about';
-    var url = 'http://www.appletuan.com';
-    var cid = req.param('cid');
-    if (cid) {
-        cid = "?cid=" + cid;
-    } else {
-        cid = "";
-    }
+	var Res = res;
+	var get_url = 'http://www.appletuan.com/page/about';
+	var url = 'http://www.appletuan.com';
+	var cid = req.param('cid');
+	if (cid) {
+		cid = "?cid=" + cid;
+	} else {
+		cid = "";
+	}
 
-    http.get(get_url, function(res) {
-        var chunks = [];
-        var size = 0;
-        res.on('data', function(chunk) {
-            chunks.push(chunk);
-            size += chunk.length;
-        });
-        res.on('end', function() {
-            var data = Buffer.concat(chunks, size);
-            var html = data.toString();
-            var $ = cheerio.load(html);
-            var date = $('#main-nav-price-report').text();
-            if (path == $('#main-nav-price-report').attr('href') && (Date.now() - result["time"]) < 3600000) {
-                console.log('getting old data success!');
-                Res.json({
-                    result: result
-                });
-            } else {
-                result = {};
-                path = $('#main-nav-price-report').attr('href');
-                console.log('path update success!path:' + path);
-                result["path"] = path;
-                result["date"] = date;
-                result["time"] = Date.now();
-                http.get(url + path + cid, function(res) {
-                    var chunks = [];
-                    var size = 0;
-                    res.on('data', function(chunk) {
-                        chunks.push(chunk);
-                        size += chunk.length;
-                    })
-                    res.on('end', function() {
-                        var data = Buffer.concat(chunks, size);
-                        var html = data.toString();
-                        var $ = cheerio.load(html);
-                        $('.header').remove();
-                        $('.product-series-heading span').text('(参考报价，根据市场价格随时更新，下单联系微信）');
-                        var content = $('.col-md-8 .box').eq(1).html();
-                        content = content.replace(/(<\/?a.*?>)/g, '');
-                        result["html"] = content;
-                        Res.json({
-                            result: result
-                        });
-                    });
-                });
-            }
-        });
-    });
+	http.get(get_url, function(res) {
+		var chunks = [];
+		var size = 0;
+		res.on('data', function(chunk) {
+			chunks.push(chunk);
+			size += chunk.length;
+		});
+		res.on('end', function() {
+			var data = Buffer.concat(chunks, size);
+			var html = data.toString();
+			var $ = cheerio.load(html);
+			var date = $('#main-nav-price-report').text();
+			if (path == $('#main-nav-price-report').attr('href') && (Date.now() - result["time"]) < 3600000) {
+				console.log('getting old data success!');
+				Res.json({
+					result: result
+				});
+			} else {
+				result = {};
+				path = $('#main-nav-price-report').attr('href');
+				console.log('path update success!path:' + path);
+				result["path"] = path;
+				result["date"] = date;
+				result["time"] = Date.now();
+				http.get(url + path + cid, function(res) {
+					var chunks = [];
+					var size = 0;
+					res.on('data', function(chunk) {
+						chunks.push(chunk);
+						size += chunk.length;
+					})
+					res.on('end', function() {
+						var data = Buffer.concat(chunks, size);
+						var html = data.toString();
+						var $ = cheerio.load(html);
+						$('.header').remove();
+						$('.product-series-heading span').text('(参考报价，根据市场价格随时更新，下单联系微信）');
+						var content = $('.col-md-8 .box').eq(1).html();
+						//content = content.replace(/(<\/?a.*?>)/g, '');
+						result["html"] = content;
+						Res.json({
+							result: result
+						});
+					});
+				});
+			}
+		});
+	});
 
 });
 
