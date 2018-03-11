d899b66bfdd139c12491845676c284a758eb319c
sunzeqi <438063442@qq.com>
2018-03-11 10:43:48 +0800

    使用request模块请求外部数据 获取报价使用promise链式调用

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 var cheerio = require('cheerio');
-var path;
+var request = require('request');
+var date, path, cid, result = {};
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {
@@ -18,6 +19,23 @@ router.get('/product_models/:id/prices', function(req, res, next) {
 	res.render('charts', {
 		title: '胖胖数码 - 趋势图'
 	});
+});
+
+router.get('/getprice', function(req, res, next) {
+	cid = req.param('cid') ? ("?cid=" + cid) : "";
+	getPath()
+		.then(() => {
+			return getPrice();
+		})
+		.then(() => {
+			resPrice(res, result);
+		})
+		.catch(e => {
+			//报价未更新，return缓存
+			if (e.notRealPromiseException) {
+				resPrice(res, e.data);
+			}
+		})
 });
 
 router.get('/getCharts', function(req, res, next) {
@@ -62,67 +80,49 @@ router.get('/getCharts', function(req, res, next) {
 	})
 });
 
-router.get('/getJobs', function(req, res, next) {
-	var result = {};
-	var Res = res;
-	var get_url = 'http://www.appletuan.com/page/about';
-	var url = 'http://www.appletuan.com';
-	var cid = req.param('cid');
-	if (cid) {
-		cid = "?cid=" + cid;
-	} else {
-		cid = "";
-	}
-
-	http.get(get_url, function(res) {
-		var chunks = [];
-		var size = 0;
-		res.on('data', function(chunk) {
-			chunks.push(chunk);
-			size += chunk.length;
-		});
-		res.on('end', function() {
-			var data = Buffer.concat(chunks, size);
-			var html = data.toString();
-			var $ = cheerio.load(html);
-			var date = $('#main-nav-price-report').text();
-			if (path == $('#main-nav-price-report').attr('href') && (Date.now() - result["time"]) < 3600000) {
-				console.log('getting old data success!');
-				Res.json({
-					result: result
-				});
-			} else {
-				result = {};
-				path = $('#main-nav-price-report').attr('href');
-				console.log('path update success!path:' + path);
-				result["path"] = path;
-				result["date"] = date;
-				result["time"] = Date.now();
-				http.get(url + path + cid, function(res) {
-					var chunks = [];
-					var size = 0;
-					res.on('data', function(chunk) {
-						chunks.push(chunk);
-						size += chunk.length;
-					})
-					res.on('end', function() {
-						var data = Buffer.concat(chunks, size);
-						var html = data.toString();
-						var $ = cheerio.load(html);
-						$('.header').remove();
-						$('.product-series-heading span').text('(参考报价，根据市场价格随时更新，下单联系微信）');
-						var content = $('.col-md-8 .box').eq(1).html();
-						//content = content.replace(/(<\/?a.*?>)/g, '');
-						result["html"] = content;
-						Res.json({
-							result: result
-						});
+const getPath = () => {
+	return new Promise((resolve, reject) => {
+		request('http://www.appletuan.com/page/about', function(err, res, data) {
+			if (!err && res.statusCode == 200) {
+				var $ = cheerio.load(data);
+				if (path == $('#main-nav-price-report').attr('href') && (Date.now() - result["time"]) < 3600000) {
+					console.log('getting old data success!');
+					reject({
+						notRealPromiseException: true,
+						data: result
 					});
-				});
+				} else {
+					path = $('#main-nav-price-report').attr('href');
+					date = $('#main-nav-price-report').text();
+					resolve();
+				}
 			}
 		});
 	});
+}
 
-});
+const getPrice = () => {
+	return new Promise((resolve, reject) => {
+		request('http://www.appletuan.com' + path + cid, function(err, res, data) {
+			if (!err && res.statusCode == 200) {
+				result["path"] = path;
+				result["date"] = date;
+				result["time"] = Date.now();
+				var $ = cheerio.load(data);
+				$('.header').remove();
+				$('.product-series-heading span').text('(参考报价，根据市场价格随时更新，下单联系微信）');
+				var content = $('.col-md-8 .box').eq(1).html();
+				result["html"] = content;
+				resolve(result);
+			}
+		});
+	});
+}
+
+const resPrice = (res, result) => {
+	res.json({
+		result: result
+	});
+}
 
 module.exports = router;
